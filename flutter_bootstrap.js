6c6abad0d41b957747b8d79c649dca12f7ede3ba
Postgram User <postgram@example.com>
2026-05-01 00:08:10 +0600

    Add real-time chatting functionality

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "145564766"
+    serviceWorkerVersion: "2848393094"
   }
 });
